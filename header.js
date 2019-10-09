0e775c06baebaeaed6b2b4bae542247770eca8fe
Andrei Marius Hutupas Antoniu <andreimantoniu@yahoo.com>
2019-10-09 21:58:16 +0200

    header initial commit

---
+++ header.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$(document).ready(function () {
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        loop: true,
+        freeMode: true,
+        loopedSlides: 5, 
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+      });
+      var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        loop:true,
+        loopedSlides: 5, 
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+          swiper: galleryThumbs,
+        },
+      });
+  });
